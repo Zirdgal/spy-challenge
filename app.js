3d27940d8c79fd6fc5a8bf4b47964a96e36a52c0
Zirdgal <zirdman@gmail.com>
2024-09-21 20:54:11 +0300

    game1

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿
+const btn1 = document.getElementById('square1');
+const btn2 = document.getElementById('square2');
+
+btn1.onclick = function() {
+    btn1.style.visibility = "hidden";
+    btn2.style.visibility = "visible";
+};
+
+btn2.onclick = function() {
+    btn2.style.visibility = "hidden";
+    btn1.style.visibility = "visible";
+};
